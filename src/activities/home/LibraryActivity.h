a4c63806fc0413eaa30bd2e47c88a17a713b304a
egor0997777-byte <egor0997777@gmail.com>
2026-08-27 22:03:08 +0300

    Add magazine and continue-reading views to Library

--- src/activities/home/LibraryActivity.h
+++ src/activities/home/LibraryActivity.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+#include "MagazineIssueStore.h"
 #include "activities/Activity.h"
 #include "util/ButtonNavigator.h"
 
@@ -13,14 +14,31 @@ class LibraryActivity final : public Activity {
     std::string title;
   };
 
+  struct MagazineSeriesEntry {
+    std::string name;
+    size_t issueCount = 0;
+    std::string latestTitle;
+  };
+
+  enum class View { Root, Magazines, Issues };
+
   ButtonNavigator buttonNavigator;
   std::vector<LibraryEntry> books;
+  std::vector<MagazineSeriesEntry> magazineSeries;
+  std::vector<MagazineIssue> visibleIssues;
   size_t selectorIndex = 0;
   bool lockNextConfirmRelease = false;
+  View view = View::Root;
+  std::string selectedSeries;
 
   void scanLibrary();
   void scanDirectory(const std::string& path);
+  void loadMagazineSeries();
+  void loadIssuesForSeries(const std::string& series);
+  bool isMagazinePath(const std::string& path) const;
+  int currentItemCount() const;
   void openSelected();
+  void goBack();
 
  public:
   explicit LibraryActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
